accae01224ac91f0cb36f71538601b3d5b71414c
nisann97 <nisann@code.edu.az>
2024-05-25 09:26:44 +0400

    Added hw

---
+++ test/Controllers/CartController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using test.Data;
+using test.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test.Controllers
+{
+    public class CartController : Controller
+    {
+            private readonly AppDbContext _context;
+
+            public CartController(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IActionResult> Index()
+            {
+                int count = await _context.Blogs.CountAsync();
+                ViewBag.count = count;
+                return View(await _context.Blogs.Where(m => !m.SoftDeleted).ToListAsync());
+            }
+
+
+            [HttpGet]
+            public async Task<IActionResult> ShowMore(int skip)
+            {
+                List<Blog> blogs = await _context.Blogs.Skip(skip).Take(3).ToListAsync();
+
+                return PartialView("_BlogsPartial", blogs);
+            }
+
+        }
+    }
+
